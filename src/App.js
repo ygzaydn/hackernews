c7e6327834cd1a262de59ad2d560f17cfbc9485e
ygzaydn <ygzaydns@github.com>
2020-08-19 23:53:16 +0300

    Toggle Button/Search Functionality

--- src/App.js
+++ src/App.js
@@ -16,12 +16,16 @@ const list = [
   }
 ];
 
+
+const isSearched = searchText => item => item.name.toLowerCase().includes(searchText.toLowerCase());
+
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       list,
-      searchText: ''
+      searchText: '',
+      isToggleOn: true
     };
     this.onDismiss = this.onDismiss.bind(this);
   }
@@ -37,15 +41,24 @@ class App extends Component {
     this.setState({searchText: text});
   }
 
+  handleText = () => {
+    this.setState({isToggleOn: !this.state.isToggleOn})
+  }
+
   render () {
     return (
       <div className="App">
+        <button onClick={this.handleText}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+        
         <form>
           <input type="text"
                   onChange={this.onChangeText} />
           <span>{this.state.searchText}</span>
         </form>
-        {this.state.list.map(el => {
+
+        {this.state.list.filter(isSearched(this.state.searchText)).map(el => {
           return (
           <div key={el.objectID}>
             <span>{el.name}</span>
