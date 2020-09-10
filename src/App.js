c92ee7f5ca82c6cf01c6df91b838985c92f68579
ygz <ygzaydns@gmail.com>
2020-09-10 17:54:49 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React , { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP ='100'
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE= 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`;
 
 const list = [
   {
@@ -60,6 +63,10 @@ class App extends Component {
 
   setSearchTopStories = (result) => {
     this.setState({result});
+    const {hits, page} = result;
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const updatedHits = [...oldHits, ...hits]
+    this.setState({hits: updatedHits, page})
   }
 
   onSearchSubmit = (event) => {
@@ -68,8 +75,8 @@ class App extends Component {
     event.preventDefault();
   }
 
-  fetchSearchTopStories = (searchTerm) => {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories = (searchTerm, page = 0) => {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(resp => resp.json())
       .then(result=> this.setSearchTopStories(result))
       .catch(err => err);
@@ -87,7 +94,9 @@ class App extends Component {
   }
 
   render () {
+    console.log(url)
     const {searchText, result } = this.state;
+    const page = (result && result.page) || 0;
     console.log(result);
     if (!result) {
       return null;
@@ -110,6 +119,11 @@ class App extends Component {
           />
           : null
           }
+          <div className="interactions">
+            <button onClick={() => this.fetchSearchTopStories(searchText, page+1)}>
+              More
+            </button>
+          </div>
         </div>
       </div>
     );
