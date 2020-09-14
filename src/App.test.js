30114f10b156ff87ca72a37204f2bcd21ba744f4
ygz <ygzaydns@gmail.com>
2020-09-14 11:02:18 +0300

    Jest test case

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
-import App from './App';
+import App, { Table } from './App';
 
 describe('App', () => {
 
@@ -17,7 +17,29 @@ describe('App', () => {
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   })
-  
+
+});
+
+describe('Table', () => {
+  const props = {
+    list: {
+      hits: [
+        {title: '1', author: '1', num_comments:'1', points: 2, objectID: 'y' },
+        {title: '2', author: '2', num_comments:'1', points: 2, objectID: 'z' },
+      ]
+    }
+  };
+
+  it ('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table {...props} />, div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component= renderer.create(<Table {...props} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
 })
 
 
